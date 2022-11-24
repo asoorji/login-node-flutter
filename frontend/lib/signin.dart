4a637d3085e47cc4f641b3ed2cd4a085b1e7f3a1
Orji <ndubuisiaso@gmail.com>
2022-11-24 17:26:09 +0100

    Login commit

---
+++ frontend/lib/signin.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:frontend/signup.dart';
+import 'package:frontend/dashboard.dart';
+import 'package:frontend/user.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+
+class Signin extends StatefulWidget {
+  Signin({Key? key}) : super(key: key);
+
+  @override
+  _SigninState createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  final _formKey = GlobalKey<FormState>();
+  Future save() async {
+    var res = await http.post(Uri.parse("http://localhost:8080/signin"),
+        headers: <String, String>{
+          'Context-Type': 'application/json;charSet=UTF-8'
+        },
+        body: <String, String>{
+          'email': user.email,
+          'password': user.password
+        });
+    print(res.body);
+    print('Hey you');
+    print(user.email);
+    print(user.password);
+    print(res.headers);
+    // ignore: use_build_context_synchronously
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => Dashboard()));
+  }
+
+  User user = User('', '');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Positioned(
+            top: 0,
+            child: SvgPicture.asset(
+              'images/top.svg',
+              width: 400,
+              height: 150,
+            )),
+        Container(
+          alignment: Alignment.center,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 150,
+                ),
+                Text(
+                  "Signin",
+                  style: GoogleFonts.pacifico(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                      color: Colors.blue),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    controller: TextEditingController(text: user.email),
+                    onChanged: (value) {
+                      user.email = value;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter something';
+                      } else if (RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                        return null;
+                      } else {
+                        return 'Enter valid email';
+                      }
+                    },
+                    decoration: InputDecoration(
+                        icon: const Icon(
+                          Icons.email,
+                          color: Colors.blue,
+                        ),
+                        hintText: 'Enter Email',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(color: Colors.blue)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(color: Colors.blue)),
+                        errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(color: Colors.red)),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(color: Colors.red))),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    controller: TextEditingController(text: user.email),
+                    onChanged: (value) {
+                      user.email = value;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter something';
+                      }
+                      return null;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        icon: const Icon(
+                          Icons.vpn_key,
+                          color: Colors.blue,
+                        ),
+                        hintText: 'Enter Password',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(color: Colors.blue)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(color: Colors.blue)),
+                        errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(color: Colors.red)),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(color: Colors.red))),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(55, 16, 16, 0),
+                  child: Container(
+                    height: 50,
+                    width: 400,
+                    child: TextButton(
+                        style: ButtonStyle(
+                            elevation: MaterialStateProperty.all(0),
+                            backgroundColor: MaterialStateProperty.all(
+                                Theme.of(context).colorScheme.secondary),
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 20)),
+                            textStyle:
+                                MaterialStateProperty.all(const TextStyle(
+                              color: Colors.black,
+                            )),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ))),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            save();
+                          } else {
+                            print("not ok");
+                          }
+                        },
+                        child: const Text(
+                          "Signin",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(95, 20, 0, 0),
+                    child: Row(
+                      children: [
+                        const Text(
+                          "Not have Account ? ",
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Signup()));
+                          },
+                          child: const Text(
+                            "Signup",
+                            style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ))
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
+  }
+}
